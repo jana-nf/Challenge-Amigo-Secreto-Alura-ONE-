1c9c61d1c509b11a39d275f38aba6e9a6c35ead9
Janaína Nascimento Feitoza <janainanascimentofeitoza@gmail.com>
2025-02-27 22:35:56 -0300

    Update app.js

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,34 +1,45 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. 
+//Aqui você deverá desenvolver a lógica para resolver o problema.
 
 let listaNomeAmigos = [];
-const amigos = ['Maria', 'Gracinha', 'Jana', 'Rodrigo', 'Herodes'];
-const indiceAleatorio = Math.floor(Math.random() * amigos.length);
-const amigoAleatorio = amigos[indiceAleatorio];
-console.log(amigoAleatorio);
 
-function exibirTextoNaTela(tag, texto) {
-    let campo = document.querySelector(tag);
-    campo.innerHTML = texto;
+function adicionarAmigo(){
+    let inputAmigo = document.getElementById("amigo");
+    let nomeAmigo = inputAmigo.value;
+
+    if(!nomeAmigo){
+        alert("Digite o nome do amigo");
+        return;
+    }
+
+    listaNomeAmigos.push(nomeAmigo);
+    inputAmigo.value = "";
+    inputAmigo.focus();
+    atualizarLista();
 }
 
-exibirTextoNaTela('h1', 'Amigo Secreto');
-exibirTextoNaTela('h2', 'Digite o nome dos seus amigos');
-exibirTextoNaTela('h3', 'Clique no botão para sortear um amigo secreto');
-document.querySelector('button').addEventListener('click', function() {
-    let nomeAmigo = document.querySelector('input[name="input-name"]').value;
-    if (nomeAmigo.trim() === '') {
-        alert('Por favor, insira um nome válido!');
-    } else {
-        listaNomeAmigos.push(nomeAmigo);
-        exibirTextoNaTela('h3', `Amigo ${nomeAmigo} adicionado!`);
+function atualizarLista(){
+    let listaAmigos = document.getElementById("lista-amigos");
+    listaAmigos.innerHTML = "";
+
+    listaNomeAmigos.forEach(function(nomeAmigo) {
+        let item = document.createElement("li");
+        item.textContent = nomeAmigo;
+        listaAmigos.appendChild(item);
+    });
+}
+
+function sortearAmigo(){
+    if(listaNomeAmigos.length === 0){
+        alert("Nenhum amigo adicionado");
+        return;
     }
-});
-function gerarNomeAleatorio() {
-    if (listaNomeAmigos.length === 0) {
-        return 'Nenhum amigo foi adicionado!';
-    }
-    const indiceAleatorio = Math.floor(Math.random() * listaNomeAmigos.length);
-    const amigoAleatorio = listaNomeAmigos[indiceAleatorio];
-    listaNomeAmigos.splice(indiceAleatorio, 1);
-    return amigoAleatorio;
+
+    let sorteado = listaNomeAmigos[Math.floor(Math.random() * listaNomeAmigos.length)];
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = `O amigo sorteado foi: ${sorteado}`;
+
+    let limparLista = document.getElementById("lista-amigos");
+    limparLista.innerHTML = "";
+    listaNomeAmigos = [];
 }
